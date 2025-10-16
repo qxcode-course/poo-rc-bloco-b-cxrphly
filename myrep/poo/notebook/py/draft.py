0c1a5fc17eed0357999a166ac269285f3506e434
halyson <halyson.php@gmail.com>
2025-10-16 09:10:49 -0300

    notebook parte 3

--- myrep/poo/notebook/py/draft.py
+++ myrep/poo/notebook/py/draft.py
@@ -9,11 +9,16 @@ class Bateria:
         self.__carga -= tempo
         if self.__carga < 0:
             self.__carga = 0
-    # def carregar?
+    def carregar(self, potencia:int , tempo:int):
+        self.__carga += potencia * tempo
+        if self.__carga > self.__capacidade:
+            self.__carga = self.__capacidade
+
+    
     def temCarga(self):
         return self.__carga > 0
     
-    
+
 class Carregador:
     def __init__(self, potencia:int):
         self.__potencia = potencia
@@ -24,13 +29,16 @@ class Notebook:
     def __init__(self):
         self.__ligado = False
         self.__bateria: Bateria | None = None
+        self.__carregador: Carregador | None = None
 
     def ligar(self):
         if self.__bateria and self.__bateria.temCarga():
             self.__ligado = True
             print("msg: Notebook ligado")
+        elif self.__carregador:
+            self.__ligado = True
         else:
-            print("fail: sem bateria")
+            print("fail: sem bateria ou carregador")
 
     def desligar(self):
         self.__ligado = False
@@ -40,20 +48,53 @@ class Notebook:
         self.__bateria = bateria
         print("msg: Bateria conectada")
 
-    def setBateria(self, bateria: Bateria):
+    def rmBateria(self, bateria: Bateria):
         self.__bateria = None
         print("msg: Bateria removida")
-
+    def setCarregador(self, carregador:Carregador):
+        self.__carregador = carregador
+        print("msg: Carregador conectado")
+    def rmCarregador(self, carregador:Carregador):
+        self.__carregador = None
+        print("msg: carregador desconectado")
     def usar(self, tempo: int):
         if not self.__ligado:
             print("fail: ligue o notebook primeiro")
             return
-
-        if not self.__bateria or not self.__bateria.temCarga():
-            print("fail: sem carga na bateria")
-            self.__ligado = False
-            return
-
         print(f"msg: Usando por {tempo} minutos")
-        self.__bateria.usingBateria(tempo)
-        print("bateria:", self.__bateria.getBateria())
+        if self.__bateria and self.__carregador:
+            self.__bateria.carregar(self.__carregador.getPotencia(), tempo)
+        elif self.__bateria:
+            self.__bateria.usingBateria(tempo)
+            if not self.__bateria.temCarga():
+                print("bateira zeradad, notebook desligado")
+                self.__ligado = False
+        elif not self.__bateria or not self.__carregador():
+            print("fail: sem bateria e sem carregador")
+            self.__ligado = False
+        if self.__bateria:
+            print(self.__bateria.getBateria)
+
+    def show(self):
+        status = "ligado" if self.__ligado else "desligado"
+        print(f"Notebook: {status}")
+        if self.__bateria or self.__carregador:
+            print(f"bateria: {self.__bateria.getBateria()}, potencia carregador: {self.__carregador.getPotencia()}")
+        else:
+            print("sem bateria")
+
+
+""" 
+def main():
+    notebook:Notebook = Notebook()
+    while True:
+        
+        line = input()
+        print("$"+line)
+        args:list[str] = line.split(" ")
+        comando:str = args[0]
+
+        match comando:
+            case "end":
+                break
+ """
