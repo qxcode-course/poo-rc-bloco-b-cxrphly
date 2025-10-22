83354fd23a18b9d782ddb447fd5d9839db5586b0
halyson <halyson.php@gmail.com>
2025-10-22 09:31:10 -0300

    Task: Motoca - done

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -10,7 +10,7 @@ class Pessoa:
         return f"{self.__name} {self.__age}"
     
 class Moto:
-    def __init__(self, power:int, time:int):
+    def __init__(self, power:int=1, time:int=0):
         self.__pessoa: Pessoa | None = None
         self.__power: int =  power
         self.__time: int = time
@@ -25,14 +25,37 @@ class Moto:
         if not self.__pessoa:
             print("fail: empty motorcycle")
         else:
-            aux = self.__pessoa
+            print(f"{self.__pessoa.getName()}:{self.__pessoa.getAge()}")
             self.__pessoa = None
-            return print(f"{aux} removed")
+    def buyTime(self, amount:int):
+        self.__time += amount
+        return
+    def driveMotoca(self, distance:int ):
+        if self.__time == 0:
+            print("fail: buy time first")
+            return
+        elif not self.__pessoa:
+            print("fail: empty motorcycle")
+            return
+        elif self.__pessoa.getAge() > 10:
+            print("fail: too old to drive")
+            return
+        else:
+            aux_time = self.__time
+            self.__time -= distance
+            if self.__time <= 0:
+                print(f"fail: time finished after {aux_time} minutes")
+                self.__time = 0
+
+    def honk(self):
+        honk = ['P','','m']
+        for i in range(self.__power):
+            honk.insert(1,"e")
+        print("".join(honk))
 
     def __str__(self):
         statusPerson = f"({self.__pessoa.getName()}:{self.__pessoa.getAge()})" if self.__pessoa else "(empty)"
         return f"power:{self.__power}, time:{self.__time}, person:{statusPerson}"
-    
 
 def main():
     moto = Moto()
@@ -44,14 +67,20 @@ def main():
         if arg[0] == "end":
             break
         if arg[0] == "init":
-            moto = Moto(1,0)
+            moto = Moto(int(arg[1]))
         elif arg[0] == "enter":
-            person = Pessoa(arg[1],arg[2])
+            person = Pessoa(arg[1],int(arg[2]))
             moto.insertPerson(person)
         elif arg[0] == "leave":
             moto.removePerson()
         elif arg[0] == "show":
             print(moto)
+        elif arg[0] == "buy":
+            moto.buyTime(int(arg[1]))
+        elif arg[0] == "drive":
+            moto.driveMotoca(int(arg[1]))
+        elif arg[0] == "honk":
+            moto.honk()
         else:
             print("comando invalido")
 
